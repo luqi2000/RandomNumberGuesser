8d3c9f87da29d4da1ce09ff2d4787b04334734f8
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-09-12 13:40:44 +0100

    implemented loop with while

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ const target = 10 + Math.round(Math.random() * 100);
 let guesses = 0;
 console.log(target);
 
-const guess = Number(prompt("Guess the number (0-100): ")); // Number will convert what we will write because in reality is a String what we type.
+while (true) {
+  const guess = Number(prompt("Guess the number (0-100): "));
 
-if (guess > target) {
-  console.log("Your guess is too high");
-} else if (guess < target) {
-  console.log("Your guess is too low");
-} else {
-  console.log("Congratulations you guessed!!!");
+  if (guess > target) {
+    console.log("Your guess is too high");
+  } else if (guess < target) {
+    console.log("Your guess is too low");
+  } else {
+    console.log("Congratulations you guessed!!!");
+    break;
+  }
 }
