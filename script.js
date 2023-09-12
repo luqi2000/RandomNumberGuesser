905723c51265722b0035f79f3a2b9b5199a6051e
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-09-12 13:37:53 +0100

    added system for user to guess a number

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const prompt = require("prompt-sync")();
+
+const target = 10 + Math.round(Math.random() * 100);
+
+let guesses = 0;
+console.log(target);
+
+const guess = Number(prompt("Guess the number (0-100): ")); // Number will convert what we will write because in reality is a String what we type.
+
+if (guess > target) {
+  console.log("Your guess is too high");
+} else if (guess < target) {
+  console.log("Your guess is too low");
+} else {
+  console.log("Congratulations you guessed!!!");
+}
